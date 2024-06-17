fd938ba1e5b6b8f04aa92278ee50c2cb49feda81
LeO-SiN <pratyushmishra82160@gmail.com>
2024-06-17 16:14:49 +0530

    commit

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test{
+  public static void main (String...args){
+    System.out.println ("Hello...!!!");
+    System.out.println ("byeeee!!!");
+    System.out.println("welcome to hell");
+    System.out.println("welcome");
+  }
+}
